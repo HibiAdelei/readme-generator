581667e61463d7d3c3836b07e571fb12198d62c5
HibiAdelei <lilyadelame@gmail.com>
2021-07-09 17:53:49 -0700

    added questions array function

--- index.js
+++ index.js
@@ -6,7 +6,93 @@ const inquirer = require("inquirer");
 
 // an array in which the user will input information to later be converted to a readme
 const questions = [
+  // title of project
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    validate: validateInput,
+},
+// Project description
+{
+    type: "input",
+    name: "description",
+    message: "Please provide a project description:",
+    validate: validateInput,
+},
 
+// Table of Contents, andling this in the markdown.js
+
+// Question for Installation
+{
+    type: "input",
+    name: "installation",
+    message: "Please enter an explanation how to install the software, or commands for the program.",
+    validate: validateInput,
+},
+
+// Question for Usage
+{
+    type: "input",
+    name: "usage",
+    message: "Please describe how we can use this program/project.",
+    validate: validateInput,
+},
+
+// Question for License 
+{
+    type: "list",
+    name: "license",
+    message: "Please select a license for this project.",
+    choices: [
+        "GNU AGPLv3",
+        "GNU GPLv3",
+        "GNU LGPLv3",
+        "Apache 2.0",
+        "Boost Software 1.0",
+        "MIT",
+        "Mozilla",
+    ],
+    validate: validateInput,
+},
+
+// Question for Contributing 
+{
+    type: "input",
+    name: "contributing",
+    message: "How can users contribute to your project.",
+    validate: validateInput,
+},
+
+// Question for Tests
+{
+    type: "input",
+    name: "tests",
+    message: "Please enter any testing instructions you would like to provide for this project.",
+    validate: validateInput,
+},
+
+// QUESTIONS section -- github 
+{
+    type: "input",
+    name: "userName",
+    message: "What is your GitHub username?",
+    validate: validateInput,
+},
+
+// QUESTIONS section -- email address
+{
+    type: "input",
+    name: "userEmail",
+    message: "What is your GitHub email address that contributors may contact?",
+    validate: function (value) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+            return true;
+        } else {
+            return "Not a valid email address. Please enter a valid email address.";
+        }
+    },
+},
 
 
 
